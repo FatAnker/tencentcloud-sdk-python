eafb8e64ae04ccd0579af3c11b817a53dfd89a22
caijianli <849384970@qq.com>
2019-04-19 16:31:59 +0800

    添加clb测试代码和不使用SDK调用云api案例

---
+++ tce/tcloud/clb/CreateForwardLBListenerRules.py
@@ -0,0 +1,56 @@
+# -*- coding: utf8 -*-
+from QcloudApi.qcloudapi import QcloudApi
+from tce.tcloud.utils.server_conf import settings
+
+# 设置需要加载的模块
+module = 'lb'
+
+# 对应接口的接口名，请参考wiki文档上对应接口的接口名
+action = 'CreateForwardLBListenerRules'
+Region = 'shanghai'
+
+total_params = settings.get(Region)
+# 云API的公共参数
+print(total_params['secretId'])
+config = {
+    'Region': Region,
+    'secretId': total_params['secretId'],
+    'secretKey': total_params['secretKey'],
+    'method': 'GET',
+    'SignatureMethod': 'HmacSHA1'
+}
+
+# 接口参数，根据实际情况填写，支持json
+# 例如数组可以 "ArrayExample": ["1","2","3"]
+# 例如字典可以 "DictExample": {"key1": "value1", "key2": "values2"}
+
+action_params = {
+    'loadBalancerId':'lb-0wqe13pg',
+    'listenerId':'lbl-rvfpnndw',
+    'rules.0.domain':'www.tencent.com',
+    'rules.0.url':'fourth',
+    'rules.0.sessionExpire':211,
+    'rules.0.healthSwitch':0,
+    'rules.0.httpHash':'ip_hash',
+    'rules.1.domain':'www.ali.com',
+    'rules.1.url':'/second',
+    'rules.1.sessionExpire':321,
+    'rules.1.healthSwitch':1,
+    'rules.1.httpHash':'ip_hash'
+
+}
+
+try:
+    service = QcloudApi(module, config)
+
+    # 请求前可以通过下面几个方法重新设置请求的secretId/secretKey/Region/method/SignatureMethod参数
+    # 重新设置请求的Region
+    # service.setRegion('shanghai')
+
+    # 打印生成的请求URL，不发起请求
+    print(service.generateUrl(action, action_params))
+    # 调用接口，发起请求，并打印返回结果
+    print(service.call(action, action_params))
+except Exception as e:
+    import traceback
+    print('traceback.format_exc():\n%s' % traceback.format_exc())
